1b5462456d4d90f05e40a869e4e6436b94ac4778
kentan88 <kentan0130@gmail.com>
2014-12-17 09:19:39 +0800

    specify state column

--- lib/aass.rb
+++ lib/aass.rb
@@ -7,7 +7,7 @@ module AASS
     base.class_eval do
       def next_state
         states = self.class.aass.states
-        current_state = self.status.to_sym
+        current_state = self.send(states[:column]).to_sym
 
         if states.keys.include?(current_state)
           possible_states = states[current_state]
@@ -19,16 +19,17 @@ module AASS
   end
 
   module ClassMethods
-    def aass(options={}, &block)
-      @aass ||= AASS::Base.new(self, options)
+    def aass(opts={}, &block)
+      @aass ||= AASS::Base.new(self, opts)
       @aass.instance_eval(&block) if block
       @aass
     end
   end
 
   class Base
-    attr_accessor :klass, :states, :state
+    attr_accessor :klass, :states, :state, :column
     def initialize(klass, opts = {})
+      @column = opts[:column]
       @klass = klass
       @states = {}
     end
@@ -40,6 +41,7 @@ module AASS
     end
 
     def to(state, opts = {})
+      @states[:column] = @column
       @states[@state] << opts.merge(to: state)
     end
   end
